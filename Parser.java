cd87861dfa4d2264633ab90ec4bcf81919ef5fef
cwc8 <cwc8@st-andrews.ac.uk>
2025-04-07 15:35:08 +0100

    Same as last commit. forgot to save

--- Parser.java
+++ Parser.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Parser {
+    //2d arraylist containing an arraylist of number of cells and there colour for row and columns
+    public static ArrayList<ArrayList<ArrayList<String>>> rows = new ArrayList<>();
+    public static ArrayList<ArrayList<ArrayList<String>>> columns = new ArrayList<>();
+    //hashmap of the colours and there hex codes
     public static HashMap<String, String> colours = new HashMap<>();
 
     //used for parsing the json
@@ -12,15 +16,28 @@ public class Parser {
     public static Integer rowOrColumnNum = 0;
     public static Integer cellNum = 0;
 
-    public static ArrayList<ArrayList<ArrayList<String>>> getRows(String fileName) throws FileNotFoundException {
-        ArrayList<ArrayList<ArrayList<String>>> rows = new ArrayList<>();
-        JsonReader reader = Json.createReader(new FileReader(fileName));
-        JsonStructure jsonst = reader.read();
-        getRowsFromTree(jsonst, null, rows);
-        return rows;
+    public static void main(String[] args) {
+        String fileName = "./Jsons/cat.json";
+        try {
+            getArrayLists(fileName);
+            if (colours.size() == 0) {
+                colours.put("COLOUR_1", "0x000000");
+            }
+            System.out.println(rows.toString());
+            System.out.println(columns.toString());
+            System.out.println(colours.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static void getRowsFromTree(JsonValue tree, String key, ArrayList<ArrayList<ArrayList<String>>> rows) {
+    public static void getArrayLists(String fileName) throws FileNotFoundException {
+        JsonReader reader = Json.createReader(new FileReader(fileName));
+        JsonStructure jsonst = reader.read();
+        getArrayListsFromTree(jsonst, null);
+    }
+
+    public static void getArrayListsFromTree(JsonValue tree, String key) {
         if (key != null) {
             if (key.equals("states")) {
                 currentArray = "colours";
@@ -36,7 +53,7 @@ public class Parser {
             case OBJECT:
                 JsonObject object = (JsonObject) tree;
                 for (String name : object.keySet())
-                    getRowsFromTree(object.get(name), name, rows);
+                    getArrayListsFromTree(object.get(name), name);
                 break;
             case ARRAY:
                 JsonArray array = (JsonArray) tree;
@@ -46,14 +63,25 @@ public class Parser {
                         rowOrColumnNum++;
                         cellNum = 0;
                     }
+                } else if (currentArray.equals("columns")) {
+                    if (key == null && tree.getValueType() == JsonValue.ValueType.ARRAY) {
+                        columns.add(new ArrayList<ArrayList<String>>());
+                        rowOrColumnNum++;
+                        cellNum = 0;
+                    }
                 }
                 for (JsonValue val : array)
-                    getRowsFromTree(val, null, rows);
+                    getArrayListsFromTree(val, null);
                 break;
             case STRING:
                 JsonString st = (JsonString) tree;
-                if (currentArray.equals("rows")) {
+                if (currentArray.equals("colours")) {
+                    colours.put(key, st.toString());
+                } else if (currentArray.equals("rows")) {
                     rows.get(rowOrColumnNum).get(cellNum).add(st.toString());
+                    cellNum++;
+                } else if (currentArray.equals("columns")) {
+                    columns.get(rowOrColumnNum).get(cellNum).add(st.toString());
                     cellNum++;
                 }
                 break;
@@ -66,132 +94,22 @@ public class Parser {
                         rows.get(rowOrColumnNum).get(cellNum).add("COLOUR_1");
                         cellNum++;
                     }
+                    if (currentArray.equals("columns")) {
+                        columns.get(rowOrColumnNum).add(new ArrayList<>());
+                        columns.get(rowOrColumnNum).get(cellNum).add(num.toString());
+                        columns.get(rowOrColumnNum).get(cellNum).add("COLOUR_1");
+                        cellNum++;
+                    }
                 } else {
                     if (currentArray.equals("rows")) {
                         rows.get(rowOrColumnNum).add(new ArrayList<>());
                         rows.get(rowOrColumnNum).get(cellNum).add(num.toString());
                     }
-                }
-                break;
-            case TRUE:
-            case FALSE:
-            case NULL:
-                break;
-        }
-    }
-
-    public static ArrayList<ArrayList<ArrayList<String>>> getColumns(String fileName) throws FileNotFoundException {
-        ArrayList<ArrayList<ArrayList<String>>> columns = new ArrayList<>();
-        JsonReader reader = Json.createReader(new FileReader(fileName));
-        JsonStructure jsonst = reader.read();
-        getColumnsFromTree(jsonst, null, columns);
-        return columns;
-    }
-
-    public static void getColumnsFromTree(JsonValue tree, String key, ArrayList<ArrayList<ArrayList<String>>> columns) {
-        if (key != null) {
-            if (key.equals("states")) {
-                currentArray = "colours";
-            } else if (key.equals("rows")) {
-                currentArray = "rows";
-                rowOrColumnNum = -1;
-            } else if (key.equals("columns")) {
-                currentArray = "columns";
-                rowOrColumnNum = -1;
-            }
-        }
-        switch (tree.getValueType()) {
-            case OBJECT:
-                JsonObject object = (JsonObject) tree;
-                for (String name : object.keySet())
-                    getRowsFromTree(object.get(name), name, columns);
-                break;
-            case ARRAY:
-                JsonArray array = (JsonArray) tree;
-                if (currentArray.equals("columns")) {
-                    if (key == null && tree.getValueType() == JsonValue.ValueType.ARRAY) {
-                        columns.add(new ArrayList<ArrayList<String>>());
-                        rowOrColumnNum++;
-                        cellNum = -1;
-                    }
-                }
-                for (JsonValue val : array)
-                    getRowsFromTree(val, null, columns);
-                break;
-            case STRING:
-                JsonString st = (JsonString) tree;
-                if (currentArray.equals("columns")) {
-                    columns.get(rowOrColumnNum).get(cellNum).add(st.toString());
-                    cellNum++;
-                }
-                break;
-            case NUMBER:
-                JsonNumber num = (JsonNumber) tree;
-                if (colours.size() == 0) {
                     if (currentArray.equals("columns")) {
-                        cellNum++;
-                        columns.get(rowOrColumnNum).add(new ArrayList<>());
-                        columns.get(rowOrColumnNum).get(cellNum).add(num.toString());
-                        columns.get(rowOrColumnNum).get(cellNum).add("COLOUR_1");
-                        
-                    }
-                } else {
-                    if (currentArray.equals("columns")) {
-                        cellNum++;
                         columns.get(rowOrColumnNum).add(new ArrayList<>());
                         columns.get(rowOrColumnNum).get(cellNum).add(num.toString());
                     }
                 }
-                break;
-            case TRUE:
-            case FALSE:
-            case NULL:
-                break;
-        }
-    }
-
-    public static HashMap<String, String> getColours(String fileName) throws FileNotFoundException {
-        HashMap<String, String> colours = new HashMap<>();
-        JsonReader reader = Json.createReader(new FileReader(fileName));
-        JsonStructure jsonst = reader.read();
-        getColoursFromTree(jsonst, null, colours);
-        if (colours.size() == 0) {
-            colours.put("COLOUR_1", "0x000000");
-        }
-        return colours;
-    }
-
-    public static void getColoursFromTree(JsonValue tree, String key, HashMap<String, String> colours) {
-        if (key != null) {
-            if (key.equals("states")) {
-                currentArray = "colours";
-            } else if (key.equals("rows")) {
-                currentArray = "rows";
-                rowOrColumnNum = -1;
-            } else if (key.equals("columns")) {
-                currentArray = "columns";
-                rowOrColumnNum = -1;
-            }
-        }
-        switch (tree.getValueType()) {
-            case OBJECT:
-                JsonObject object = (JsonObject) tree;
-                for (String name : object.keySet())
-                    getColoursFromTree(object.get(name), name, colours);
-                break;
-            case ARRAY:
-                JsonArray array = (JsonArray) tree;
-                for (JsonValue val : array)
-                    getColoursFromTree(val, null, colours);
-                break;
-            case STRING:
-                JsonString st = (JsonString) tree;
-                if (currentArray.equals("colours")) {
-                    colours.put(key, st.toString());
-                    cellNum++;
-                }
-                break;
-            case NUMBER:
                 break;
             case TRUE:
             case FALSE:
