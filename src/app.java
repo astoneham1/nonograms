c0859e3756f2051d515f7d01d7b8fd5a5fb500db
Alessandro Stoneham <amps1@st-andrews.ac.uk>
2025-04-12 00:28:05 +0100

    Feature: Added some core UI components

---
+++ src/app.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class app {
+    private JPanel mainPanel;
+    private JLabel titleText;
+    private JPanel loader;
+    private JButton loadFile;
+    private JButton reset;
+    private JPanel grid;
+}
