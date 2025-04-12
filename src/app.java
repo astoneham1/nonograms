6b3f7fa7f291e6a3de975b466c23e2b7a7a25791
Alessandro Stoneham <amps1@st-andrews.ac.uk>
2025-04-12 01:11:03 +0100

    Feature: Added the 3 basic colours

--- src/app.java
+++ src/app.java
@@ -7,4 +7,13 @@ public class app {
     private JButton loadFile;
     private JButton reset;
     private JPanel grid;
+    private JPanel colorGuide;
+    private JPanel extraColors;
+    private JPanel defaultColours;
+    private JPanel color1;
+    private JLabel color1Label;
+    private JLabel color3Label;
+    private JLabel color2Label;
+    private JPanel color3;
+    private JPanel color2;
 }
