7c8e4d7080c5acd470518f03aed59ddcd3a0aab4
Alessandro Stoneham <amps1@st-andrews.ac.uk>
2025-04-12 02:08:29 +0100

    Feature: Added a main method that shows the JFrame on screen

--- src/app.java
+++ src/app.java
@@ -21,6 +21,22 @@ public class app {
     public app() {
         loadFile.addActionListener(e -> {
             // Code to be executed when the button is clicked
+            JOptionPane.showMessageDialog(mainPanel, "Load File Clicked!");
+        });
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Nonograms");
+            frame.setContentPane(new app().mainPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
     }
 }
