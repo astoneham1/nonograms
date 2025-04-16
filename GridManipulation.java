c15d22f0a14b9501ee8cc7f8af2a7ed1b821164d
Ksenia Baatz <kcb20@st-andrews.ac.uk>
2025-04-16 09:25:31 +0100

    added method to convert a grid into an array of clues

--- GridManipulation.java
+++ GridManipulation.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 
 
 public class GridManipulation {
-
-    // Prior to checking a grid, it needs to have it's progress be converted to equivalent 'clues' and have these loaded into it's clues
-    // for now b/w
     public static void convertToClues(Grid g) {
         for (int i = 0; i < g.rows; i ++) {
             // int currentColor = 0;
@@ -43,56 +40,5 @@ public class GridManipulation {
             Clue c = new Clue(count, color);
             g.columnClues.add(c);
         }
-        
-        // for (int i = 0; i < g.columns; i ++) {
-        //     int currentColor = 0;
-        //     int currentCount = 0;
-        //     ArrayList<Integer> color = new ArrayList<>();
-        //     ArrayList<Integer> count = new ArrayList<>();
-        //     for (int j = 0; j < g.rows; j++) {
-        //         if (g.grid[i][j] != 0 && g.grid[i][j] != currentColor) { 
-        //             //create a clue obj that had the current color, current count
-        //             count.add(currentCount);
-        //             color.add(currentColor);
-        //             currentColor = g.grid[i][j]; 
-        //         }
-        //         if (g.grid[i][j] == currentColor) { currentCount++; }
-        //         else { continue; }
-        //     }
-        //     Clue c = new Clue(count, color);
-        //     g.rowClues.add(c);
-        // }
     }
 }
-
-
-//  || (j == 0 && g.grid[i][j] != g.grid[i][j+1] )
-                // if (g.grid[i][j] != 0) {
-                //     currentColor = g.grid[i][j];
-                // }
-                // if (g.grid[i][j] == currentColor && currentColor != 0) {
-                //     currentCount++;
-                // }
-                // if (g.grid[i][j] != currentColor || (j>0 && g.grid[i][j-1] != 0 && g.grid[i][j] != g.grid[i][j-1])) {
-                //     count.add(currentCount);
-                //     color.add(currentColor);
-                //     currentCount = 0;
-                //     currentColor = 0;
-                // }
-
-
-                // if (g.grid[i][j] != currentColor && g.grid[i][j] != 0 ||  currentColor != 0 && g.grid[i][j] == 0) {
-                //     count.add(currentCount);
-                //     color.add(currentColor);
-                //     currentCount = 1;
-                // }
-                // if (g.grid[i][j] != 0) {
-                //     currentColor = g.grid[i][j]; 
-                //     currentCount++;
-            //         count.add(currentCount);
-            //         color.add(currentColor);
-            //         currentCount = 1;
-                // }
-
-            //     if (g.grid[i][j] == currentColor && g.grid[i][j] != 0) { currentCount++; }
-                // else { continue; }
