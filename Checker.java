54fcc13705b2367fb9834c0f375cc76b50ef8d8c
Thomas Soiza <trs3@st-andrews.ac.uk>
2025-04-24 20:54:27 +0100

    cleaned javadocs in checker and added some more test cases

--- Checker.java
+++ Checker.java
@@ -3,19 +3,20 @@ import java.util.Arrays;
 
 public class Checker {
     /**
-     * Loops through the number of rows, and calls the checkRow() method for that given row, and for the corresponding clue.
+     * Loops through the number of rows and columns, and calls the checkLine() method for that given row or column, and for the corresponding clue.
+     * Any incorrect row is added to an ArrayList (and same for a column), and these are added to another ArrayList and is returned.
      * @param grid              The player's nonogram.
-     * @param loadedGrid        List of row clues.
+     * @param rowClues          List of row clues.
      * @param columnClues       List of column clues.
-     * @return                  List of lists of coordinates where the incorrect cell is located.
+     * @return                  List of lists of all incorrect rows and columns (or empty otherwise)
      */
     public ArrayList<ArrayList<Integer>> checkNonogram(int[][] grid, ArrayList<Clue> rowClues, ArrayList<Clue> columnClues) {
-        // Where incorrect rows and columns are kept.
+        // Where incorrect rows and columns are stored.
         ArrayList<Integer> incorrectRows = new ArrayList<Integer>();
         ArrayList<Integer> incorrectColumns = new ArrayList<Integer>();
-        ArrayList<ArrayList<Integer>> incorrectRowsAndColumns = new ArrayList<ArrayList<Integer>>(); // this is what gets returned
+        ArrayList<ArrayList<Integer>> incorrectRowsAndColumns = new ArrayList<ArrayList<Integer>>();
 
-        // For each row in the 2D array grid, check the ith row against the ith clue.
+        // For each row in the 2D array grid, check the ith row against the ith clue. If it is not correct, add to the corresponding list.
         for (int i = 0; i < grid.length; i++) {
             boolean correctRow = checkLine(grid[i], rowClues.get(i));
             if (!correctRow) {
@@ -23,7 +24,7 @@ public class Checker {
             }
         }
         for (int j = 0; j < grid[0].length; j++) {
-            // Get the ith column, and check it against the corresponding clue.
+            // Get the ith column, and check it against the ith clue.
             int[] column = getColumn(grid, j);
             boolean correctColumn = checkLine(column, columnClues.get(j));
             if (!correctColumn) {
@@ -39,10 +40,9 @@ public class Checker {
      * This checks a specific line abides with a specific clue.
      * @param line          The row or column which is being checked.
      * @param clue          The clue which the row or column is being checked against.
-     * @return              List of coordinates of incorrect cells.
+     * @return              True or false, depending on whether the line met the constraints of the clue.
      */
     public boolean checkLine(int[] line, Clue clue) {
-        // For global access within the method
         ArrayList<Integer> counts = clue.getCounts();       // Gets the list of counts for this clue.
         ArrayList<Integer> colours = clue.getColours();     // Gets the list of colours for this clue, given as integers.
         int numCorrect = 0;         // Keeps track of the number of cells which are correct for a given clue.
@@ -61,7 +61,6 @@ public class Checker {
         }
 
         // Loops through the size of the counts list. The length of the counts and colours lists must be the same.
-        // Example counts list: [5, 3, 2] which is 5 squares, a space, 3 squares, a space, 2 squares, a space.
         for (int j = 0; j < counts.size(); j++) {
             numCorrect = 0;
             numSquares = counts.get(j);
@@ -71,10 +70,9 @@ public class Checker {
             for (int i = positionInLine; i < line.length; i++) {
                 // Checks if the number of squares matches the clue count, which signals that that part of the clue is complete.
                 if (numCorrect == numSquares) {
-                    if (line[i] != squareColour) { // The cell MUST be a different colour, otherwise the clue has not been satisfied (e.g. the clue is 4 but there are 5 filled in)
+                    if (line[i] != squareColour) { // The cell MUST be a different colour, otherwise the clue has not been satisfied.
                         if (j != counts.size() - 1) {
-                            break; // Break only if not on the last part of the clue, so clues like (5, 3, 2) will keep going.
-                                   // Fixes 0, 0, 0, 2, 2, 0 and clue is 3. Allows for clues with multiple counts.
+                            break; // Break only if there are still parts of the clue left to be checked.
                         }
                     }
                     else {
@@ -88,8 +86,7 @@ public class Checker {
                 }
  
                 // The cell is incorrect if it is any colour other than the square colour, (except blank or unknown),
-                //  or if it is blank or unknown but the clue has not been finished (i.e. 22222 is correct, but 220222 is NOT)
-                // e.g. Clue was 5 black squares with no spaces but the player entered 2 black, one space, 2 black.
+                //  or if it is blank or unknown but the part of the clue has not been finished yet.
                 else if ((line[i] != 1 && line[i] != 0) || ((line[i] == 1 || line[i] == 0) && numCorrect > 0 && numSquares != numCorrect)) {
                     return false;
                 }
@@ -109,9 +106,9 @@ public class Checker {
  
     /**
      * From the 2D array, gets the column as an int array so only one 'checkLine' method is required. This gets all elements
-     * for a fixed row number, e.g. grid[i][0], grid[i][1], grid[i][2] where i is every element per column and 0,1,2... is the column number.
+     * for a passed in column number by looping through the length of the column (i.e. the number of rows, or the length of the 2D array)
      * @param grid          The 2D array player grid
-     * @param num           The column number, i.e. get all elements from this column.
+     * @param num           The column number
      * @return              An integer array which is the ith column from the player grid.
      */
     public int[] getColumn(int[][] grid, int num) {
@@ -125,11 +122,19 @@ public class Checker {
         return column;
     }
 
+    /**
+     * Creates the String which has the number of rows and columns which are incorrect (if any) and the user's current progress for their grid.
+     * @param incorrectLines        The list of lists of incorrect rows and columns, obtained from checkNonogram().
+     * @param rows                  The number of rows the grid has (also the length of the row clues list) which is used for calculating the progress.
+     * @param columns               The number of columns the grid has (also the length of the column clues list) which is used for calculating the progress.
+     * @return                      The String which is output to the user when they have checked their puzzle.
+     */
     public String getMessage(ArrayList<ArrayList<Integer>> incorrectLines, int rows, int columns) {
         StringBuilder sb = new StringBuilder();
         ArrayList<Integer> incorrectRows = incorrectLines.get(0);
         ArrayList<Integer> incorrectColumns = incorrectLines.get(1);
 
+        // If there are no incorrect rows or lines, add the appropriate message/s.
         if (incorrectRows.size() == 0) {
             sb.append("All rows are correct.\n");
         }
@@ -143,21 +148,31 @@ public class Checker {
             return congrats;
         }
 
-        for (int i : incorrectRows) {
-            int rowNum = i+1;
+        // Loop through every incorrect row and column and add this to the message.
+        for (int rowIndex : incorrectRows) {
+            int rowNum = rowIndex + 1;
             sb.append("Incorrect row " + rowNum + "\n");
         }
-        for (int c : incorrectColumns) {
-            int columnNum = c+1;
+        for (int columnIndex : incorrectColumns) {
+            int columnNum = columnIndex + 1;
             sb.append("Incorrect column " + columnNum + "\n");
         }
 
+        // Calculate the progress and add this to the message.
         double progress = getProgress(incorrectRows.size(), incorrectColumns.size(), rows, columns);
         sb.append("Completion: " + progress + "%");
         String error = sb.toString();
         return error;
     }
 
+    /**
+     * Calculates how many rows and columns the user has correct for a given grid.
+     * @param incorrectRows             The number of incorrect rows.
+     * @param incorrectColumns          The number of incorrect columns.
+     * @param rows                      The total number of rows.
+     * @param columns                   The total number of columns.
+     * @return                          The percentage of completion of the user's grid.
+     */
     public double getProgress(int incorrectRows, int incorrectColumns, int rows, int columns) {
         int totalCorrectRows = rows - incorrectRows;
         int totalCorrectColumns = columns - incorrectColumns;
